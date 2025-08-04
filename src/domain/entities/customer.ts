532db84f9df341c2367787e8205fef0335dc7df0
Ferdinando Jr <juniorferdinando@gmail.com>
2025-08-04 09:22:24 -0300

    0.0.1
    Iniciando projeto com duas rotas para customer

---
+++ src/domain/entities/customer.ts
@@ -0,0 +1,25 @@
+import { Address } from "../valueObjects/address";
+
+export type CustomerType = 'person' | 'company'
+
+export interface Customer {
+  customerId: number 
+  customerCode: string
+  address: Address
+  email: { value: string }
+  phones: { value: string }[]
+  isActive: boolean
+  createdAt: string
+  runtimeType: CustomerType
+}
+
+export interface PersonCustomer extends Customer {
+  fullName: string
+  cpf: { value: string }
+}
+
+export interface CompanyCustomer extends Customer {
+  legalName: string
+  tradeName: string
+  cnpj: { value: string }
+}
---
+++ src/domain/valueObjects/address.ts
@@ -0,0 +1,6 @@
+export interface Address {
+    state: string,
+    city: string,
+    street: string,
+    cep: { value: string }
+}
